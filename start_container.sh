d17587bd36e2eb05db6979bd8fda083b4ddd0e9c
Yoshihiro Okuda <yookuda@nig.ac.jp>
2019-11-06 17:08:22 +0900

    modify start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-<<<<<<< HEAD
-CONTAINER_HOME="/home/y-okuda/git/singularity_postgresql"
-=======
 CONTAINER_HOME="/home/user/singularity_postgresql"
->>>>>>> 0809e3ff618f4d74c079f3c1c9a029e3446a8d35
 IMAGE="ubuntu-18.04-postgresql-12.0.simg"
 INSTANCE="pgsql"
 PORT="55432"
@@ -24,12 +20,13 @@ if [ ! -e ${CONTAINER_HOME}/data ]; then
     ${INSTANCE}
     singularity exec instance://${INSTANCE} initdb -D /usr/local/pgsql12/data --encoding=UTF-8 --no-locale
     singularity instance.stop ${INSTANCE}
+
+    # 設定ファイルのコピー
+    cp ${CONTAINER_HOME}/pg_hba.conf ${CONTAINER_HOME}/data/
+    cp ${CONTAINER_HOME}/pg_ident.conf ${CONTAINER_HOME}/data/
+    cp ${CONTAINER_HOME}/postgresql.conf ${CONTAINER_HOME}/data/
 fi
 
-# 設定ファイルのコピー
-cp ${CONTAINER_HOME}/pg_hba.conf ${CONTAINER_HOME}/data/
-cp ${CONTAINER_HOME}/pg_ident.conf ${CONTAINER_HOME}/data/
-cp ${CONTAINER_HOME}/postgresql.conf ${CONTAINER_HOME}/data/
 
 # instance起動
 singularity instance.start \
@@ -38,13 +35,5 @@ singularity instance.start \
 ${CONTAINER_HOME}/${IMAGE} \
 ${INSTANCE}
 
-<<<<<<< HEAD
 # postgresqlサーバ起動
 singularity exec instance://${INSTANCE} pg_ctl -D /usr/local/pgsql12/data -l /usr/local/pgsql12/logs/logfile -o "-p ${PORT}" start
-=======
-if [ ! -e ${CONTAINER_HOME}/pgsql_data/db_data ]; then
-    singularity exec instance://${INSTANCE} initdb -D /usr/local/pgsql12/data/db_data --encoding=UTF-8 --no-locale
-fi
-
-singularity exec instance://${INSTANCE} pg_ctl -D /usr/local/pgsql12/data/db_data -l /usr/local/pgsql12/logs/logfile -o "-p ${PORT}" start
->>>>>>> 0809e3ff618f4d74c079f3c1c9a029e3446a8d35
