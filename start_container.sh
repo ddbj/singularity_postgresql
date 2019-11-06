53094d2d2091ffc8a8a82fc0f58c36473de20c4a
okuda <yookuda@nig.ac.jp>
2019-11-06 12:24:49 +0900

    add files

---
+++ start_container.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+CONTAINER_HOME="/home/okuda/singularity/ubuntu-18.04-postgresql-12.0"
+IMAGE="ubuntu-18.04-postgresql-12.0.simg"
+INSTANCE="pgsql"
+PORT="50001"
+
+if [ ! -e ${CONTAINER_HOME}/logs ]; then
+    mkdir ${CONTAINER_HOME}/logs
+fi
+
+singularity instance.start \
+-B ${CONTAINER_HOME}/pgsql_data:/usr/local/pgsql12/data \
+-B ${CONTAINER_HOME}/logs:/usr/local/pgsql12/logs \
+${CONTAINER_HOME}/${IMAGE} \
+${INSTANCE}
+
+if [ ! -e ${CONTAINER_HOME}/pgsql_data/db_data ]; then
+    singularity exec instance://${INSTANCE} initdb -D /usr/local/pgsql12/data/db_data --no-locale
+fi
+
+singularity exec instance://${INSTANCE} pg_ctl -D /usr/local/pgsql12/data/db_data -l /usr/local/pgsql12/logs/logfile -o "-p ${PORT}" start
